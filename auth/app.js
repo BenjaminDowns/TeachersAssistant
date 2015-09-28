1a892abe7f311a6d2156523bddf82fadf7b8f5d1
Benjamin Downs <benjamin.downs@gmail.com>
2015-09-28 23:28:05 +0200

    basic, non-encrypted auth functional

--- auth/app.js
+++ auth/app.js
@@ -4,21 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+app.use(logger('dev'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,6 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  app.locals.pretty = true;
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- auth/routes/index.js
+++ auth/routes/index.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Teacher = mongoose.model('Teacher', new Schema( {
+	id: ObjectId,
+	firstName: String,
+	lastName: String,
+	email: {type: String, unique: true},
+	password: String,
+}))
+
+mongoose.connect('mongodb://localhost/auth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,11 +22,45 @@ router.get('/', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
 	res.render('login.jade')
+})
+
+router.post('/login', function(req, res, next) {
+	Teacher.findOne({ email: req.body.email }, function(err, teacher) {
+		if (!teacher) {
+			res.render('login.jade', {error: 'Invalid email or password.'});
+		} else {
+			if (req.body.password === teacher.password) {
+				res.redirect('/dashboard');
+			} else {
+				res.render('login.jade', {error: 'Invalid email or password.'});
+			}
+		}
+	})
 });
 
 router.get('/register', function(req, res, next) {
 	res.render('register.jade')
 });
+
+router.post('/register', function(req, res, next) {
+	var teacher = new Teacher({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: req.body.password
+	})
+	teacher.save(function(err) {
+		if (err) {
+			var err = 'Something bad happened! Please try again!';
+			if (err.code === 11000) {
+				err = "That email is already taken. Please register with another."
+			}
+			res.render('register.jade', { error: err })
+		} else {
+			res.redirect('/dashboard');
+		}
+	})
+})
 
 router.get('/dashboard', function(req, res, next) {
 	res.render('dashboard.jade')
